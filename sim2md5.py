457bb2cf7894fa86298f4dbb2b680fc1fd8ce921
cuile <i@cuile.com>
2018-09-24 01:26:04 +0800

    主文件重命名

--- sim2md5.py
+++ sim2md5.py
@@ -3,18 +3,13 @@ import hashlib
 import argparse
 
 parser = argparse.ArgumentParser(description='''生成手机号对应的MD5''')
-parser.add_argument('-f', '--logFileName', metavar='logFileName', help='生成的日志文件')
+# parser.add_argument('-f', '--logFileName', metavar='logFileName', help='生成的日志文件')
 parser.add_argument('-r', '--range', metavar='range', help='手机号号段')
 args = parser.parse_args()
 
 LOG_FORMAT = "%(message)s"
-logging.basicConfig(filename=args.logFileName, level=logging.INFO, format=LOG_FORMAT)
-# logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+logging.basicConfig(filename=args.range + '.csv', level=logging.INFO, format=LOG_FORMAT)
 
-r = int(args.range)
-start = r * 100000000
-end = (r + 1) * 100000000 - 1
-
-for i in range(start, end):
+for i in range(int(args.range) * 100000000, (int(args.range) + 1) * 100000000):
     phone_number = str(i)
     logging.info(phone_number + "," + hashlib.md5(phone_number.encode(encoding='utf-8')).hexdigest())
