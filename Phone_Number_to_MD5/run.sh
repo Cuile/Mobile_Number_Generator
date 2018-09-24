65df3f65be34d878ed959fec4ad2f750eb4f8b92
cuile <i@cuile.com>
2018-09-24 23:47:11 +0800

    移动运行脚本

--- Phone_Number_to_MD5/run.sh
+++ Phone_Number_to_MD5/run.sh
@@ -2,6 +2,7 @@ nohup ./sim2md5 -r 189 > 189.log 2>&1 && \
         ./sim2md5 -r 199 > 199.log 2>&1 && \
         ./sim2md5 -r 181 > 181.log 2>&1 && \
         ./sim2md5 -r 180 > 180.log 2>&1 && \
-        ./sim2md5 -r 177 > 177.log 2>&1 && \
-        ./sim2md5 -r 173 > 173.log 2>&1 && \
+        
+nohup   ./sim2md5 -r 177 > 177.log 2>&1 && ^
+        ./sim2md5 -r 173 > 173.log 2>&1 && ^
         ./sim2md5 -r 153 > 153.log 2>&1 &
