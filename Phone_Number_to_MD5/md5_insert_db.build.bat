2e7c35f4b159de3d3d4160e841c91b265ea818fe
cuile <i@cuile.com>
2018-09-27 11:05:11 +0800

    删除 MD5数据插入数据脚本，改用LOAD DATA

--- Phone_Number_to_MD5/md5_insert_db.build.bat
+++
@@ -1,7 +0,0 @@
-docker container run ^
-        -it ^
-        --rm ^
-        -v "%cd%":/root ^
-        -w /root ^
-        python-dev-env:1 ^
-        pyinstaller --onefile --name md5_insert_db --paths . --noconfirm --clean md5_insert_db.py
