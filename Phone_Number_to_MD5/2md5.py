e2a74980b8ed60ef89dd095561e9dd8e217923e1
cuile <i@cuile.com>
2018-09-30 23:48:43 +0800

    添加帮助信息

--- Phone_Number_to_MD5/2md5.py
+++ Phone_Number_to_MD5/2md5.py
@@ -3,9 +3,13 @@
 import logging
 import hashlib
 import argparse
+import textwrap
 
-parser = argparse.ArgumentParser(description='''生成手机号对应的MD5''')
-parser.add_argument('-r', '--range', metavar='range', help='手机号号段')
+parser = argparse.ArgumentParser(description=textwrap.dedent('''生成手机号对应的MD5'''),
+                                 epilog="前台使用：./2md5 -r 133 \n"
+                                        "后台使用：nohup ./2md5 -r 133 > 133.log 2>&1 & \n",
+                                 formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('-r', metavar='range', help='手机号号段')
 args = parser.parse_args()
 
 LOG_FORMAT = "%(message)s"
