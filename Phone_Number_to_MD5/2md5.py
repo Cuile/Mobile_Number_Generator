c28b3fc81330c8acd6d1ed2f44c8dc4a84bf3416
cuile <i@cuile.com>
2018-10-22 23:01:13 +0800

    升级代码 可同时支持号段与文件

--- Phone_Number_to_MD5/2md5.py
+++ Phone_Number_to_MD5/2md5.py
@@ -1,22 +1,40 @@
 # coding:utf-8
 
-import logging
-import hashlib
 import argparse
+import csv
+import hashlib
+import io
+import logging
+import os
 import textwrap
 
-parser = argparse.ArgumentParser(description=textwrap.dedent('''生成手机号对应的MD5'''),
-                                 epilog="前台使用：./2md5 -r 133 \n"
-                                        "后台使用：nohup ./2md5 -r 133 > 133.log 2>&1 & \n",
-                                 formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-r', metavar='range', help='手机号号段')
+parser = argparse.ArgumentParser(description=textwrap.dedent('''
+                                                            生成手机号对应的MD5\n
+                                                            ./2md5 -r 133\n
+                                                            ./2md5 -f xxx
+                                                            '''), formatter_class=argparse.RawTextHelpFormatter)
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-r', metavar="range", dest="range", type=int, help='手机号号段')
+group.add_argument("-f", metavar="file", dest="file", type=str, help="包含手机号的文件")
 args = parser.parse_args()
 
 LOG_FORMAT = "%(message)s"
-logging.basicConfig(filename=args.range + '.csv', level=logging.INFO, format=LOG_FORMAT)
+if args.range:
+    logging.basicConfig(filename=str(args.range) + '.csv', level=logging.INFO, format=LOG_FORMAT)
 
-s = int(args.range) * 100000000
-e = (int(args.range) + 1) * 100000000
-for i in range(s, e):
-    phone_number = str(i)
-    logging.info(phone_number + "," + hashlib.md5(phone_number.encode(encoding='utf-8')).hexdigest())
+    s = int(args.range) * 100000000
+    e = (int(args.range) + 1) * 100000000
+    for i in range(s, e):
+        phone_number = str(i)
+        logging.info(phone_number + "," + hashlib.md5(phone_number.encode(encoding='utf-8')).hexdigest())
+
+elif args.file:
+    (filename, extension) = os.path.splitext(args.file)
+    logging.basicConfig(filename=filename + '.md5', level=logging.INFO, format=LOG_FORMAT)
+    with io.open(args.file, 'rt')as rf:
+        csv_reader = csv.reader(rf)
+        for r in csv_reader:
+            logging.info(hashlib.md5(r[0].encode(encoding='utf-8')).hexdigest())
+
+else:
+    print("usage: -h or --help arguments show help message")
