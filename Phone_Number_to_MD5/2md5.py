57c29c53faee6cd0d906bdc0b8d7f0c26cfb306e
cuile <i@cuile.com>
2018-10-29 22:55:31 +0800

    优化代码
    改为csv库写入文件，提高运行效率
    加入通用计时函数，打印代码运行时间

--- Phone_Number_to_MD5/2md5.py
+++ Phone_Number_to_MD5/2md5.py
@@ -4,37 +4,49 @@ import argparse
 import csv
 import hashlib
 import io
-import logging
 import os
 import textwrap
 
-parser = argparse.ArgumentParser(description=textwrap.dedent('''
-                                                            生成手机号对应的MD5\n
-                                                            ./2md5 -r 133\n
-                                                            ./2md5 -f xxx
-                                                            '''), formatter_class=argparse.RawTextHelpFormatter)
+import common
+
+parser = argparse.ArgumentParser(description=textwrap.dedent(
+    '''
+    生成手机号对应的MD5
+    
+    将3位手机号段内的所有号码转为MD5码，生成“3位号段+.csv”的文件
+    shell: ./2md5 -r 133
+    result: 133.csv 
+
+    或 
+
+    将文本文件内包含的手机号（每行一个）转为MD5码，生成“文本文件名+.md5”的文件
+    shell: ./2md5 -f xxx
+    result: xxx.md5
+    '''
+), formatter_class=argparse.RawTextHelpFormatter)
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-r', metavar="range", dest="range", type=int, help='手机号号段')
 group.add_argument("-f", metavar="file", dest="file", type=str, help="包含手机号的文件")
 args = parser.parse_args()
 
-LOG_FORMAT = "%(message)s"
+common.timing_starts()
 if args.range:
-    logging.basicConfig(filename=str(args.range) + '.csv', level=logging.INFO, format=LOG_FORMAT)
-
     s = int(args.range) * 100000000
     e = (int(args.range) + 1) * 100000000
-    for i in range(s, e):
-        phone_number = str(i)
-        logging.info(phone_number + "," + hashlib.md5(phone_number.encode(encoding='utf-8')).hexdigest())
+    with io.open(str(args.range) + '.csv', 'wt') as f:
+        csv_writer = csv.writer(f)
+        for i in range(s, e):
+            pn = str(i)
+            csv_writer.writerow([pn, hashlib.md5(pn.encode(encoding='utf-8')).hexdigest()])
 
 elif args.file:
     (filename, extension) = os.path.splitext(args.file)
-    logging.basicConfig(filename=filename + '.md5', level=logging.INFO, format=LOG_FORMAT)
-    with io.open(args.file, 'rt')as rf:
+    with io.open(args.file, 'rt')as rf, io.open(filename + ".md5", 'wt')as wf:
         csv_reader = csv.reader(rf)
+        csv_writer = csv.writer(wf)
         for r in csv_reader:
-            logging.info(hashlib.md5(r[0].encode(encoding='utf-8')).hexdigest())
+            csv_writer.writerow(hashlib.md5(r[0].encode(encoding='utf-8')).hexdigest().upper())
 
 else:
     print("usage: -h or --help arguments show help message")
+common.timing_ends()
---
+++ Phone_Number_to_MD5/common.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import time
+
+start = startTime = end = endTime = bar = None
+
+
+def timing_starts():
+    global start, startTime
+    start = time.clock()
+    startTime = time.time()
+
+
+def timing_ends():
+    global end, endTime
+    end = time.clock()
+    endTime = time.time()
+    print("-------------------------------------------------------------------------")
+    print("CPU Running time: %fs" % (end - start))
+    print("Script Running time: %fs" % (endTime - startTime))
+    print("-------------------------------------------------------------------------")
