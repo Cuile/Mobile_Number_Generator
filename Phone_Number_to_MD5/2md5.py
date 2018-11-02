b53123593e4100e7dd520006a2ce313ca3507e6d
cuile <i@cuile.com>
2018-11-02 14:09:42 +0800

    添加 哈希算法选项

--- Phone_Number_to_MD5/2md5.py
+++ Phone_Number_to_MD5/2md5.py
@@ -11,23 +11,28 @@ import common
 
 parser = argparse.ArgumentParser(description=textwrap.dedent(
     '''
-    生成手机号对应的MD5
+    生成手机号对应的哈希值
     
-    将3位手机号段内的所有号码转为MD5码，生成“3位号段+.csv”的文件
-    shell: ./2md5 -r 133
+    1.将3位手机号段内的所有号码生成MD5码，生成“3位号段+.csv”的文件
+    shell: ./2md5 -r 133 md5
     result: 133.csv 
 
-    或 
-
-    将文本文件内包含的手机号（每行一个）转为MD5码，生成“文本文件名+.md5”的文件
-    shell: ./2md5 -f xxx
+    2.将文本文件内包含的手机号（每行一个）生成sha256码，生成“文本文件名+.sha256”的文件
+    shell: ./2md5 -f xxx sha256
     result: xxx.md5
     '''
 ), formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("h", metavar="hash", type=str, help="摘要算法，md5、sha1、sha224、sha256、sha384、sha512")
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-r', metavar="range", dest="range", type=int, help='手机号号段')
 group.add_argument("-f", metavar="file", dest="file", type=str, help="包含手机号的文件")
 args = parser.parse_args()
+hashlib.algorithms_available
+
+
+def getHash(hash: str, value: str):
+    return eval("hashlib.{hash}('{value}'.encode(encoding='utf-8')).hexdigest().upper()".format(hash=hash, value=value))
+
 
 common.timing_starts()
 if args.range:
@@ -37,16 +42,16 @@ if args.range:
         csv_writer = csv.writer(f)
         for i in range(s, e):
             pn = str(i)
-            csv_writer.writerow([pn, hashlib.md5(pn.encode(encoding='utf-8')).hexdigest()])
-
+            csv_writer.writerow([pn, getHash(args.h, pn)])
 elif args.file:
     (filename, extension) = os.path.splitext(args.file)
     with io.open(args.file, 'rt')as rf, io.open(filename + ".md5", 'wt')as wf:
         csv_reader = csv.reader(rf)
         csv_writer = csv.writer(wf)
         for r in csv_reader:
-            csv_writer.writerow(hashlib.md5(r[0].encode(encoding='utf-8')).hexdigest().upper())
-
+            csv_writer.writerow(getHash(args.h, r[0]))
+elif not args.h:
+    print("usage: -h or --help arguments show help message")
 else:
     print("usage: -h or --help arguments show help message")
 common.timing_ends()
