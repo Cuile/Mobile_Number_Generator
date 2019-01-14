00fb77649ebcb36d3bf1ce1f38e17e5a9a3754a7
cuile <i@cuile.com>
2019-01-15 01:43:12 +0800

    Update debug.bat
    修改导入目录

--- debug.bat
+++ debug.bat
@@ -2,7 +2,7 @@
 docker container run ^
         -it ^
         --rm ^
-        -v "%cd%":/root ^
+        -v "%cd%"\src:/root/code ^
         -v D:\Data_Label\:/root/data ^
         -w /root/code ^
         python-dev-env:1
