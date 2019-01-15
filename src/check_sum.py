9047ddb05871ad1da9efeedadc801e9c35589b1c
cuile <i@cuile.com>
2019-01-15 19:04:38 +0800

    Update check_sum.py
    修改函数名，优化调用方式

--- src/check_sum.py
+++ src/check_sum.py
@@ -18,22 +18,27 @@ def make_it_complete(imei_str: str):
     return ''.join([imei_str, str(check_code)])
 
 
-def imei(imei: str):
-    # 判断 imei 为14位数字
-    if re.match(r'^\d{14}$', imei):
-        # 返回 True,完整的imei
-        return {'check': True, 'imei': make_it_complete(str(imei))}
-    # 判断 imei 为15位数字
-    elif re.match(r'^\d{15}$', imei):
-        # 取前14位数字，进行校验
-        complete_imei = make_it_complete(re.match(r'^(\d{14})\d$', imei).group(1))
-        # 判断校验后的值是否相等
-        if imei == complete_imei:
-            # 返回 True,原始的imei
-            return {'check': True, 'imei': imei}
+def getCheckCode(type: str, imei: str):
+    if type == 'imei':
+        # 判断 imei 为14位数字
+        if re.match(r'^\d{14}$', imei):
+            # 返回 True,完整的imei
+            return {'check': True, 'imei': make_it_complete(str(imei))}
+        # 判断 imei 为15位数字
+        elif re.match(r'^\d{15}$', imei):
+            # 取前14位数字，进行校验
+            complete_imei = make_it_complete(re.match(r'^(\d{14})\d$', imei).group(1))
+            # 判断校验后的值是否相等
+            if imei == complete_imei:
+                # 返回 True,原始的imei
+                return {'check': True, 'imei': imei}
+            else:
+                # 返回 False,正确的imei
+                return {'check': False, 'imei': complete_imei}
         else:
-            # 返回 False,正确的imei
-            return {'check': False, 'imei': complete_imei}
+            # 返回 False,imei为None
+            return {'check': False, 'imei': None}
+    elif type == 'emid':
+        pass
     else:
-        # 返回 False,imei为None
-        return {'check': False, 'imei': None}
+        raise RuntimeError('getCheckCode type error')
