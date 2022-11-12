b2cd2db0d72e7e9d0e3cc7ada126947d732d4bab
Cuile <i@cuile.com>
2022-11-12 15:54:23 +0800

    修改启动、关闭命令

--- debug.bat
+++ debug.bat
@@ -1,8 +1,3 @@
-:: debug:
-docker container run ^
-        -it ^
-        --rm ^
-        -v "%cd%"\src:/root/code ^
-        -v D:\Data_Label\:/root/data ^
-        -w /root/code ^
-        md5.collision:1
+# /bin/bash
+
+docker exec -it mobile_number_generator /bin/sh
