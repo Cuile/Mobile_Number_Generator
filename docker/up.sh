18357553d178d5b4a9f2a235a4bac6c1b5145caa
Cuile <i@cuile.com>
2023-05-31 14:22:59 +0800

    Update up.sh

--- docker/up.sh
+++ docker/up.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 
 # cp ../src/requirements.txt .
-docker compose -f start.yml up -d
+docker compose -f start.yml run --rm mobile_number_generator
 # rm requirements.txt
 # docker system prune
