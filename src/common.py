94e8d948c5fe83dd6d21303477206dfffff2942a
Cuile <i@cuile.com>
2023-05-31 16:50:19 +0800

    Update common.py

--- src/common.py
+++ src/common.py
@@ -49,9 +49,11 @@ def make_ragne(r: int, path: str, hash=None):
 def make_random(path: str):
     csvfiles = [name for name in os.listdir(path)
                 if name.endswith('.csv')]
+    # for fp in csvfiles:
+    #     csvfp.append = open(path + '/' + fp, 'r')
     for i in range(600000000):
         file = path + '/' + random.choice(csvfiles)
-        with open(file, 'r') as f:
+        # with open(file, 'r') as f:
         # with open('tmp.csv', 'a') as t:
-            row = os.popen('sed -n {}p {}'.format(random.randint(1, 100000000), file)).read(11)
-            print(row)
+        row = os.popen('sed -n {}p {}'.format(random.randint(1, 100000000), file)).read(11)
+        print(row)
