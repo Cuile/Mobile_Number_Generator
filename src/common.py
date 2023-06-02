51f5e3df344e374b38fc40f0ed19c8b23a2fec8e
Cuile <i@cuile.com>
2023-06-02 21:02:51 +0800

    Update common.py

--- src/common.py
+++ src/common.py
@@ -96,7 +96,7 @@ def make_random(path: str):
             
             # 每次随机读取的号码个数
             print('待处理行号 {}~{}'.format(1, max_lineno))
-            if (step <= max_lineno):
+            if (step <= int(max_lineno)):
                 step /= 10
             print(step)
             for f in csv_files:
