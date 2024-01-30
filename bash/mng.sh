477c46d792930525d443c0ce33b8f1c9064caed4
Cuile <i@cuile.com>
2024-01-30 20:57:05 +0800

    Update mng.sh

--- bash/mng.sh
+++ bash/mng.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-docker compose -f ../docker/build.yml \
-                build --ssh=~/.ssh/config \
+docker compose -f ../docker/build.yml build \
                 && \
-docker compose -f ../docker/start.yml \
-                run --rm \
+docker compose -f ../docker/start.yml run \
+                --rm \
                 --entrypoint "python main.py" \
                 mobile_number_generator \
                 "$@"
